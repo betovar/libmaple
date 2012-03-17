b56e854ec03ba8e275134a6c1eae1d81e7fb1abf
Brian E Tovar <betovar@leaflabs.com>
2012-03-16 20:40:38 -0400

    updated test file with demo

--- examples/test-fft.cpp
+++ examples/test-fft.cpp
@@ -9,20 +9,26 @@
 #include "wirish.h"
 #include "MapleFFT.h"
 
-uint16 count = 0;
+//Number of points 
+#define N 64
+
+//Input and Output arrays
+uint32 x[N],y[N];
+
+//Real and Imaginary arrays
+uint16 real[N], imag[N];
 
 void setup() {
-    pinMode(BOARD_LED_PIN, OUTPUT);
-    digitalWrite(BOARD_LED_PIN, HIGH);
-
     Serial1.begin(9600);
     Serial1.println("**** Beginning FFT test");
-
-
 }
 
 void loop() {
-
+    for(i=0; i<N; i++) {
+        x[i] = (((uint16)(real[i])) | ((uint32)(imag[i]<<16))); 
+    }
+    //Computes the FFT of the x[N] samples
+    cr4_fft_64_stm32(y, x, N);
 }
 
 __attribute__((constructor)) void premain() {
